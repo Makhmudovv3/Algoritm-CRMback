d65b3358eb38aed34edf88bc8d8b8c3f29411755
Makhmudovv3 <makhmudovazizbek712@gmail.com>
2026-07-21 15:49:02 +0500

    Fix event creation missing createdBy

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,10 +1,24 @@
 const BaseController = require('./baseController');
 const eventService = require('../services/eventService');
+const logger = require('../utils/logger');
+const { errorResponse } = require('../utils/response');
 
 class EventController extends BaseController {
   constructor() {
     super(eventService, 'Event');
   }
+
+  create = async (req, res) => {
+    try {
+      if (req.user && req.user.id) {
+        req.body.createdBy = req.user.id;
+      }
+      return await super.create(req, res);
+    } catch (error) {
+      logger.error(`Error in Event create: ${error.message}`);
+      return errorResponse(res, `Failed to create event`, [error.message], 400);
+    }
+  };
 }
 
 module.exports = new EventController();
