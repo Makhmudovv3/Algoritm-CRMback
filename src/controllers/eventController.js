d96260510bd7bda14d812ef1eec124a6f3c7ee56
Makhmudovv3 <makhmudovazizbek712@gmail.com>
2026-07-21 16:00:57 +0500

    Fix TypeError on super.create by using inline implementation

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,7 +1,8 @@
 const BaseController = require('./baseController');
 const eventService = require('../services/eventService');
 const logger = require('../utils/logger');
-const { errorResponse } = require('../utils/response');
+const { errorResponse, successResponse } = require('../utils/response');
+const notificationService = require('../services/notificationService');
 
 class EventController extends BaseController {
   constructor() {
@@ -13,7 +14,19 @@ class EventController extends BaseController {
       if (req.user && req.user.id) {
         req.body.createdBy = req.user.id;
       }
-      return await super.create(req, res);
+      
+      const record = await this.service.create(req.body, req.user?.id, req.ip);
+      const io = req.app.get('io');
+      
+      await notificationService.notifyAdmins(io, {
+        title: `New Event Created`,
+        content: `A new event has been created.`,
+        type: 'system',
+        priority: 'medium',
+        category: 'general'
+      });
+
+      return successResponse(res, `Event created successfully`, record, {}, 201);
     } catch (error) {
       logger.error(`Error in Event create: ${error.message}`);
       return errorResponse(res, `Failed to create event`, [error.message], 400);
