afb1a7de9a1646c24d083a6934f7b58ab6eedc39
Makhmudovv3 <makhmudovazizbek712@gmail.com>
2026-07-22 16:52:00 +0500

    fix(settings): use correct controller for settings routes to match frontend expected structure and resolve 500 error

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const settingsController = require('../controllers/SettingsController');
+const systemSettingController = require('../controllers/systemSettingController');
 const { verifyToken, verifyRole } = require('../middlewares/authMiddleware');
 
-router.get('/', verifyToken, settingsController.getSettings);
-router.get('/:id', verifyToken, settingsController.getById);
-router.post('/', verifyToken, verifyRole(['SUPER_ADMIN']), settingsController.create);
-router.put('/:key', verifyToken, verifyRole(['SUPER_ADMIN']), settingsController.updateSetting);
-router.delete('/:id', verifyToken, verifyRole(['SUPER_ADMIN']), settingsController.delete);
+router.get('/', verifyToken, systemSettingController.getAll);
+router.get('/:category', verifyToken, systemSettingController.getByCategory);
+router.put('/:category', verifyToken, verifyRole(['SUPER_ADMIN']), systemSettingController.updateCategory);
 
 module.exports = router;
